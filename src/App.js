669af33b2e3668f4d724905cbd60936e72d49875
Aarushi Jaiswal <jaiswalaarushi20@gmail.com>
2021-06-11 01:47:23 +0530

    covidwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const Statewise = () => {
+  const [data, setData] = useState([]);
+
+  const getCovidData = async () => {
+    const res = await fetch("https://api.covid19india.org/data.json");
+    const actualData = await res.json();
+    console.log(actualData.statewise);
+    setData(actualData.statewise);
+  };
+
+  useEffect(() => {
+    getCovidData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container-fluid mt-5">
+        <div className="main-heading">
+          <h1 className="mb-5 text-center">
+            {" "}
+            <span className="font-weight-bold">🔴 LIVE </span>COVID-19 DASHBOARD
+          </h1>
+        </div>
 
-export default App;
+        <div className="box-container">
+          <div className="table-responsive">
+            <table className="table table-hover">
+              <thead className="thead-dark">
+                <tr>
+                  <th> State </th>
+                  <th> Confirmed </th>
+                  <th> recovered </th>
+                  <th> deaths </th>
+                  <th> active </th>
+                  <th> updated </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((curElem, ind) => {
+                  return (
+                    <tr key={ind}>
+                      <th> {curElem.state} </th>
+                      <td> {curElem.confirmed} </td>
+                      <td> {curElem.recovered} </td>
+                      <td> {curElem.deaths} </td>
+                      <td> {curElem.active} </td>
+                      <td> {curElem.lastupdatedtime} </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Statewise;
